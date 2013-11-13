6033983c259629205458b015b3b920168197e4b1
tarVolcano <tarVolcano@users.noreply.github.com>
2013-11-14 00:46:20 +0900

    Added Sublime text2 plugin

--- modules/people/manifests/tarVolcano.pp
+++ modules/people/manifests/tarVolcano.pp
@@ -32,19 +32,42 @@ class people::tarVolcano {
       'vagrant-omnibus',
     ]:
   }
+  
+  # Sublime text2プラグイン
+  sublime_text_2::package {
+    'ConvertToUTF8':
+      source => 'seanliang/ConvertToUTF8';
+    'LineEndings':
+      source => 'SublimeText/LineEndings';
+    'Markdown Preview':
+      source => 'revolunet/sublimetext-markdown-preview';
+    'VBScript':
+      source => 'SublimeText/VBScript';
+    ##'Package Control':
+    ##　source => 'wbond/sublime_package_control';
+    ##　source => 'wbond/sublime_alignment';
+    ##↑どっちもうまく入らない、ソースが違うのかな？以下エラーメッセージ
+    ##Wrapped exception:
+    ##Could not match 　source
+    ##Error: Could not match 　source at /opt/boxen/repo/modules/people/manifests/tarvolcano.pp:47 on node tars-mba.local
+
+  
+  }
 
   ###################
   # osxの設定(https://github.com/boxen/puppet-osx)
   ###################
   # +------- global --------+
-  include osx::global::enable_keyboard_control_access # ON :キーボードでのコントロール
+  #include osx::global::enable_keyboard_control_access # ON :キーボードでのコントロール
   include osx::global::disable_autocorrect            # OFF:自動スペル補正
-  class { 'osx::global::key_repeat_delay':            # キーをおしっぱにして連続入力されるまでのdelay
-    delay => 10                                       # を10ミリ秒に
-  }
-  class { 'osx::global::key_repeat_rate':             # キーを押しっぱなしにしてして連続入力される間隔
-    rate => 2                                         # を2ミリ秒に
-  }
+  include osx::global::key_repeat_delay               # Set the default value (35)
+  #class { 'osx::global::key_repeat_delay':           # キーをおしっぱにして連続入力されるまでのdelay
+  #  delay => 10                                      # を10ミリ秒に
+  #}
+  include osx::global::key_repeat_rate                # Set the default value (0)
+  #class { 'osx::global::key_repeat_rate':            # キーを押しっぱなしにしてして連続入力される間隔
+  #  rate => 5                                        # を5ミリ秒に
+  #}
 
 
   $home     = "/Users/${::boxen_user}"
